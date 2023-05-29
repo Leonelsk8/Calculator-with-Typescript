1799ba4a84ebfc942ebd20579442f757955c4796
Leonelsk8 <leonelgomez.sk8@gmail>
2023-05-29 00:20:45 -0300

    calculator updating

--- app/app.js
+++ app/app.js
@@ -1,82 +1,69 @@
 var cont = [];
 var number = '';
+var ON = false;
 var buttons = document.querySelectorAll('.buttonsSelect');
 var buttonsArray = Array.from(buttons);
 var concate = function (value) {
     cont.push(value);
 };
-//buttonsArray.map((butt)=>{
-//  butt.addEventListener('click', ()=>{
-//    const valor: string = butt.textContent === null ? '' :  butt.textContent;
-//    if(valor !== '+' && valor !== '-' && valor !== 'x' && valor !== '/'){
-//      number = number + valor;
-//      const element = document.querySelector('#resultLed');
-//      const pElement = document.createElement('p');
-//      pElement.innerHTML = valor
-//      element !== null ? element.appendChild(pElement) : '';
-//    }else{
-//      concate(parseInt(number));
-//      concate(valor);
-//      number = '';
-//      const element = document.querySelector('#resultLed');
-//      const pElement = document.createElement('p');
-//      pElement.innerHTML = valor
-//      element !== null ?  element.appendChild(pElement) : '';
-//    }
-//  })
-//})
 buttonsArray.map(function (butt) {
     butt.addEventListener('click', function () {
-        var valor = butt.textContent === null ? '' : butt.textContent;
-        var element = document.querySelector('#resultLed');
-        var pElement = document.createElement('p');
-        pElement.innerHTML = valor;
-        element !== null ? element.appendChild(pElement) : '';
+        if (ON) {
+            var valor = butt.textContent === null ? '' : butt.textContent;
+            var element = document.querySelector('#resultLed');
+            var pElement = document.createElement('p');
+            pElement.innerHTML = valor;
+            element !== null ? element.appendChild(pElement) : '';
+        }
     });
 });
-//const calculate = ()=>{
-//  if(!isNaN(cont[0]) && number !== ''){
-//    concate(parseInt(number));
-//    let result = cont[0];
-//    for(let i=1; i<cont.length ; i++){
-//        result = cont[i] === '+' ? result + cont[i+1] : result;
-//        result = cont[i] === '-' ? result - cont[i+1] : result;
-//        result = cont[i] === 'x' ? result * cont[i+1] : result;
-//        result = cont[i] === '/' ? result / cont[i+1] : result;
-//    }
-//    const element = document.querySelector('#resultLed');
-//    element? element.innerHTML = `<p>${result}</p>`: '';
-//    cont = [];
-//    number = result.toString();
-//  }else{
-//    alert('No puedes iniciar o terminar con un opererador!!');
-//    const element = document.querySelector('#resultLed');
-//    element? element.innerHTML = '': '';
-//    cont = [];
-//    number = '';
-//  }
-//}
 var calculate = function () {
     var nodo = document.querySelector('#resultLed');
     var childrens = nodo === null || nodo === void 0 ? void 0 : nodo.children;
     var arrayChild = Array.from(childrens ? childrens : []);
     arrayChild.map(function (item) {
         var valor = item.textContent === null ? '' : item.textContent;
-        if (valor !== '+' && valor !== '-' && valor !== 'x' && valor !== '/') {
-            number = number + valor;
-        }
-        else {
-            concate(parseInt(number));
-            concate(valor);
-            number = '';
+        if (valor !== '|') {
+            if (valor !== '+' && valor !== '-' && valor !== 'x' && valor !== '/') {
+                number = number + valor;
+            }
+            else {
+                concate(parseInt(number));
+                concate(valor);
+                number = '';
+            }
         }
     });
     if (!isNaN(cont[0]) && number !== '') {
         concate(parseInt(number));
         var result = cont[0];
         for (var i = 1; i < cont.length; i++) {
-            result = cont[i] === '+' ? result + cont[i + 1] : result;
-            result = cont[i] === '-' ? result - cont[i + 1] : result;
+            if (cont[i] === '+') {
+                if (cont[i + 2] === 'x') {
+                    result = result + (cont[i + 1] * cont[i + 3]);
+                    i = i + 3;
+                }
+                else if (cont[i + 2] === '/') {
+                    result = result + (cont[i + 1] / cont[i + 3]);
+                    i = i + 3;
+                }
+                else {
+                    result = result + cont[i + 1];
+                }
+            }
+            if (cont[i] === '-') {
+                if (cont[i + 2] === 'x') {
+                    result = result - (cont[i + 1] * cont[i + 3]);
+                    i = i + 3;
+                }
+                else if (cont[i + 2] === '/') {
+                    result = result - (cont[i + 1] / cont[i + 3]);
+                    i = i + 3;
+                }
+                else {
+                    result = result - cont[i + 1];
+                }
+            }
             result = cont[i] === 'x' ? result * cont[i + 1] : result;
             result = cont[i] === '/' ? result / cont[i + 1] : result;
         }
@@ -86,7 +73,7 @@ var calculate = function () {
         number = '';
     }
     else {
-        alert('No puedes iniciar o terminar con un opererador!!');
+        alert('No puedes iniciar o terminar con un operador!!');
         var element = document.querySelector('#resultLed');
         element ? element.innerHTML = '' : '';
         cont = [];
@@ -98,14 +85,15 @@ var OnAc = function () {
     element ? element.innerHTML = '' : '';
     cont = [];
     number = '';
+    if (!ON) {
+        ON = true;
+        var elementOn = document.createElement('div');
+        elementOn.className = 'parpadeo';
+        elementOn.innerHTML = '|';
+        var led = document.querySelector('#led');
+        led === null || led === void 0 ? void 0 : led.appendChild(elementOn);
+    }
 };
-//const deleteNum = ()=>{
-//  const nodo = document.querySelector('#resultLed');
-//  nodo?.removeChild(nodo.lastChild ? nodo.lastChild : nodo);
-//  if(cont.length > 0){
-//    cont.pop();
-//  }
-//}
 var deleteNum = function () {
     var nodo = document.querySelector('#resultLed');
     nodo === null || nodo === void 0 ? void 0 : nodo.removeChild(nodo.lastChild ? nodo.lastChild : nodo);
--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 let cont: any[] = [];
 let number: string = '';
+let ON = false;
 
 const buttons = document.querySelectorAll('.buttonsSelect');
 const buttonsArray = Array.from(buttons);
@@ -8,36 +9,73 @@ const concate = (value: any)=>{
   cont.push(value);
 }
 
+buttonsArray.map((butt)=> {
+  butt.addEventListener('click', ()=> {
+    if(ON){
+      let valor = butt.textContent === null ? '' : butt.textContent;
+      let element = document.querySelector('#resultLed');
+      let pElement = document.createElement('p');
+      pElement.innerHTML = valor;
+      element !== null ? element.appendChild(pElement) : '';
+    }
+  });
+});
+
 const calculate = ()=>{
   const nodo = document.querySelector('#resultLed');
   const childrens = nodo?.children;
   const arrayChild = Array.from(childrens? childrens : []);
   arrayChild.map((item)=>{
     const valor: string = item.textContent === null ? '' :  item.textContent;
-    if(valor !== '+' && valor !== '-' && valor !== 'x' && valor !== '/'){
-      number = number + valor;
-    }else{
-      concate(parseInt(number));
-      concate(valor);
-      number = '';
+    if(valor !== '|'){
+      if(valor !== '+' && valor !== '-' && valor !== 'x' && valor !== '/'){
+        number = number + valor;
+      }else{
+        concate(parseInt(number));
+        concate(valor);
+        number = '';
+      }
     }
   });
 
   if(!isNaN(cont[0]) && number !== ''){
     concate(parseInt(number));
     let result = cont[0];
+
     for(let i=1; i<cont.length ; i++){
-        result = cont[i] === '+' ? result + cont[i+1] : result;
-        result = cont[i] === '-' ? result - cont[i+1] : result;
-        result = cont[i] === 'x' ? result * cont[i+1] : result;
-        result = cont[i] === '/' ? result / cont[i+1] : result;
+      if(cont[i] === '+' ){
+        if(cont[i+2]=== 'x'){
+          result = result + (cont[i+1] * cont[i+3]);
+          i= i+3;
+        }else if(cont[i+2]=== '/'){
+          result = result + (cont[i+1] / cont[i+3]);
+          i= i+3;
+        }else{
+          result = result + cont[i+1];
+        }
+      }
+
+      if(cont[i] === '-' ){
+        if(cont[i+2]=== 'x'){
+          result = result - (cont[i+1] * cont[i+3]);
+          i= i+3;
+        }else if(cont[i+2]=== '/'){
+          result = result - (cont[i+1] / cont[i+3]);
+          i= i+3;
+        }else{
+          result = result - cont[i+1];
+        }
+      }
+      result = cont[i] === 'x' ? result * cont[i+1] : result;
+      result = cont[i] === '/' ? result / cont[i+1] : result;
     }
+
     const element = document.querySelector('#resultLed');
     element? element.innerHTML = `<p>${result}</p>`: '';
     cont = [];
     number = '';
   }else{
-    alert('No puedes iniciar o terminar con un opererador!!');
+    alert('No puedes iniciar o terminar con un operador!!');
     const element = document.querySelector('#resultLed');
     element? element.innerHTML = '': '';
     cont = [];
@@ -48,9 +86,18 @@ const calculate = ()=>{
 
 const OnAc = ()=>{
   const element = document.querySelector('#resultLed');
-  element? element.innerHTML = '': '';
+  element?element.innerHTML = '':'';
   cont = [];
   number = '';
+  
+  if(!ON){
+    ON = true;
+    const elementOn = document.createElement('div');
+    elementOn.className = 'parpadeo';
+    elementOn.innerHTML  = '|';
+    const led = document.querySelector('#led');
+    led?.appendChild(elementOn);
+  }
 }
 
 const deleteNum = ()=>{
